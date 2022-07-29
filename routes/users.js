cbea41b1e4febb4f3cee692886f44707e18a3001
eliashhtores <eliashhtorres@gmail.com>
2022-07-29 17:52:44 -0500

    WIP users module

--- routes/users.js
+++ routes/users.js
@@ -9,7 +9,10 @@ app.use(express.json())
 router.get('/', async (req, res) => {
     try {
         const users =
-            await pool.query(`SELECT 1`)
+            await pool.query(`SELECT us.id, username, name 
+            FROM user us
+            JOIN role ro ON (ro.id = us.role_id)
+            WHERE active`)
         res.json(users[0])
     } catch (error) {
         res.status(500).json({ message: error.message })
@@ -18,7 +21,25 @@ router.get('/', async (req, res) => {
 })
 
 router.post('/', (req, res) => {
-    res.send('POST request to role path')
+    res.send('POST request to users path')
+})
+
+// Validate user
+router.post('/validate', async (req, res) => {
+    try {
+        const { username, password } = req.body
+        const user = await pool.query('SELECT id, role_id, username, name FROM user WHERE username = ? AND password = MD5(?) AND active', [username, password])
+
+        if (user[0].length == 0) {
+            res.status(404).json({ 'message': 'User not found', 'status': 404 })
+            return
+        }
+
+        res.json(user[0][0])
+    } catch (error) {
+        res.status(500).json({ message: error.message, status: 500 })
+        console.error(error.message)
+    }
 })
 
 module.exports = router
