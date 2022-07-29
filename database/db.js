2a09043a85b3d0ae738fc74c1f69dac0a690a805
eliashhtorres <eliashhtorres@gmail.com>
2022-07-29 08:30:15 -0500

    Added DB support

---
+++ database/db.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql2/promise')
+require('dotenv').config()
+
+try {
+    const pool = mysql.createPool({
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        host: process.env.DATABASE_HOST,
+        database: process.env.DATABASE_NAME,
+    })
+    console.log(`Connected to database ${process.env.DATABASE_NAME}...`)
+    module.exports = pool
+} catch (error) {
+    console.error(error)
+}
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const router = express.Router()
+const app = express()
+const pool = require('../database/db')
 
-router.get('/', (req, res) => {
-  res.send('GET request to role path')
+app.use(express.json())
+
+// Get all users
+router.get('/', async (req, res) => {
+    try {
+        const users =
+            await pool.query(`SELECT 1`)
+        res.json(users[0])
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+        console.error(error.message)
+    }
 })
 
 router.post('/', (req, res) => {
-  res.send('POST request to role path')
+    res.send('POST request to role path')
 })
 
 module.exports = router
